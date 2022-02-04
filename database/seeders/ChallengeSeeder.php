a88583c9fc25d307b3db4f646a413dabadbc83f2
Daniel Sepp <seppdaniel@gmail.com>
2022-02-04 23:27:06 +0200

    Add seeding option

--- database/seeders/ChallengeSeeder.php
+++ database/seeders/ChallengeSeeder.php
@@ -2,12 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Challenge;
 use Illuminate\Database\Seeder;
 
 class ChallengeSeeder extends Seeder
 {
     public function run()
     {
-        //
+        Challenge::factory()
+            ->count(58)
+            ->create();
     }
 }
